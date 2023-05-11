c9135be723662c9db4a754cbfa017d04d9e7c1cd
jruimot0704 <122780882+jruimot0704@users.noreply.github.com>
2023-05-11 12:19:41 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    cuteBot.motors(50, 20)
+})
+input.onButtonPressed(Button.AB, function () {
+    cuteBot.motors(50, 50)
+})
+input.onButtonPressed(Button.B, function () {
+    cuteBot.motors(20, 50)
+})
+basic.forever(function () {
+	
+})
